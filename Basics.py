b86df8225be396277936c209afc849edd41e00c5
Sidra Ghaffar <sidraghaffar04@gmail.com>
2026-01-21 22:19:43 +0000

    Create Basics.py with example print statements
    Added a simple Python program demonstrating various print statements, variable assignments, and formatted strings.

---
+++ Basics.py
@@ -0,0 +1,45 @@
+
+#simple program
+print("Hello world")
+print("Welcome to python world")
+# variable asssigned kiya hain
+name="Sidra"
+age=21
+print(name,age)
+#end='',next print in same line
+print("Sidra",end= ' ')
+print("Ghaffar")
+# Name or favourite color print
+name="Sidra"
+color="Black"
+print("My name is", name, "and favourite Color is ",color)
+#first and last name same line ma print
+firstName="Sidra"
+lastName="Ghaffar"
+print(firstName,end=' ')
+print(lastName)
+# 1 se 5 numbers same line ma print
+print(1,2,3,4,5)
+print(1,end=' ')
+print(2,end=' ')
+print(3,end=' ')
+print(4,end=' ')
+print(5,end=' ')
+print(6)
+#apna name ,surname,age ak line ma print use of +
+print("Sidra"+" ""Ghaffar"+" ""21")
+# city or country ko print by using variables
+city="Burewala"
+country="Pakistan"
+print("I live in ",city,"which is a part of ",country)
+# f string ka use
+a=10
+b=5
+print(f"sum of the {a} and {b} is {a+b}")
+print(f"subtract of the {a} and {b} is {a-b}")
+print(f"product of the {a} and {b} is {a*b}")
+# floating point formatting
+pi=3.14159265
+print(f"pi value up to 2 decimals:{pi:.2f}")
+print(type(pi))
+print(type(a))
